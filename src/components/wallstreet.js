d94a933cf80de5351f220813c7928c4c27fa9120
caritobd91 <cborman@doeanderson.com>
2019-07-19 10:42:08 -0400

    [add] wallstreet page to show other news articles from different api call

---
+++ src/components/wallstreet.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+
+const url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=cb83495bb9724d3c80337804facdd7de"
+
+class Wallstreet extends Component {
+  constructor() {
+    super();
+    this.state = { articles: [], displayedArticles: [], isOpen: false };
+  }
+
+  async getWallstreet() {
+    let result = await fetch(url).then(response => response.json());
+
+    console.log('wall', result);
+    return result.articles;
+  }
+
+  async componentWillMount() {
+    this.getWallstreet().then(
+      article => this.setState({articles: article})
+    ).then(() => {
+      this.setDisplayedArticles();
+    });
+  }
+
+  hasNewsLimit() {
+    return this.props.newsLimit > 0;
+  }
+
+  setDisplayedArticles() {
+    if (this.hasNewsLimit()) {
+      this.setState({isOpen: false, displayedArticles: this.state.articles.slice(0, this.props.newsLimit)});
+    }
+    else {
+      this.setState({displayedArticles: this.state.articles});
+    }
+  }
+
+  toggleArticles = () => {
+    if (!this.state.isOpen)
+      this.setState({ displayedArticles: this.state.articles, isOpen: true });
+    else {
+      this.setDisplayedArticles();
+      window.scrollTo(0, 0);
+    }
+  }
+
+  toggleArticlesButtonText = () => {
+    return this.state.isOpen ? 'See Less Articles' : 'See All Articles';
+  }
+
+  render() {
+    console.log('displayed articles ', this.state.displayedArticles);
+    return(
+      <div>
+        <div className="articles">
+          {this.state.displayedArticles.map((article, key) =>
+            <div className="news" key={ article.title }>
+              <div className="news-image" style={{backgroundImage: `url(${article.urlToImage})`}}></div>
+              <h1>{ article.title }</h1>
+              <p>{ article.description }</p>
+            </div>
+          )}
+        </div>
+        <button className="button-all" onClick={this.toggleArticles}>{this.toggleArticlesButtonText()}</button>
+        </div>
+    )
+  }
+}
+
+export default Wallstreet;
