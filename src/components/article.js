c6e710c576f30d326b82d13efb29d69d6449ddc6
caritobd91 <cborman@doeanderson.com>
2019-07-19 10:42:31 -0400

    [refactor] started refactoring articles markup in its own component

--- src/components/article.js
+++ src/components/article.js
@@ -1,7 +1,26 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class Article extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
+  hasNewsLimit() {
+    return this.props.newsLimit > 0;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="news" key={ this.article.title }>
+          <div className="news-image" style={{backgroundImage: `url(${this.article.urlToImage})`}}></div>
+          <h1>{ this.article.title }</h1>
+          <p>{ this.article.description }</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Article;
