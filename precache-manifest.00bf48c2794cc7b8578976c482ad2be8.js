df0c3fc31c3e87bfb90e42c8f7616d8f89b79882
caritobd91 <cborman@doeanderson.com>
2019-10-20 13:42:01 -0400

    Updates

---
+++ precache-manifest.00bf48c2794cc7b8578976c482ad2be8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60be24a93a1f9aea9e87de30de23d53",
+    "url": "/news-app-react/index.html"
+  },
+  {
+    "revision": "d4e5f065120210fefc61",
+    "url": "/news-app-react/static/css/main.11873361.chunk.css"
+  },
+  {
+    "revision": "f304d515c10ba0689b1f",
+    "url": "/news-app-react/static/js/2.c921f1ae.chunk.js"
+  },
+  {
+    "revision": "d4e5f065120210fefc61",
+    "url": "/news-app-react/static/js/main.72c9acbf.chunk.js"
+  },
+  {
+    "revision": "7c19a015893d5c0b4b24",
+    "url": "/news-app-react/static/js/runtime~main.1a47af19.js"
+  }
+]);
